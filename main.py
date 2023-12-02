32f8a503f19c8b99359d7cc276e6d80f6e0230db
saffa19 <duncanvdw1@gmail.com>
2023-12-02 21:44:38 +0000

    pandas everywhere

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import websockets
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point, Polygon, LineString
 import pylab as pl
 from datetime import datetime, timezone, date
 from pathlib import Path
 
-def tracker(msg):
+def tracker(msg:json):
     Path(f'./tracking/{date.today()}').mkdir(parents=True, exist_ok=True)
     path = f'./tracking/{date.today()}/{msg["MetaData"]["MMSI"]}.json'
     entry = msg['Message']['PositionReport'] | msg['MetaData']
@@ -28,14 +28,43 @@ def tracker(msg):
             json.dump(data, file, indent = 4)
 
 
-def mapper(vessel_id):
-    with open(f'./tracking/{date.today()}/{vessel_id}.json', 'r') as file:
-        data = json.load(file)
-    df = pd.DataFrame(data) #better way of constructing geo df? 
-    geo_df = gpd.GeoDataFrame(data, geometry=[Point(xy) for xy in zip(df['Longitude'], df['Latitude'])])
-    #export as shapefile (1 shapefile per ship per day)
+def mapper(vessel_id:int):
+    #with open(f'./tracking/{date.today()}/{vessel_id}.json', 'r') as file:
+    #    data = json.load(file)
+    #df = pd.DataFrame(data)
+    df = pd.read_json(f'./tracking/{date.today()}/{vessel_id}.json')
+    geometry = [Point(xy) for xy in zip(df['Longitude'], df['Latitude'])]   #create points from separate latlongs 
+    geo_df = gpd.GeoDataFrame(df, geometry=geometry)
+
+    #points shp (1 shapefile per ship per day)
     Path(f'./shapefiles/{date.today()}/{vessel_id}/').mkdir(parents=True, exist_ok=True)
     geo_df.to_file(f'./shapefiles/{date.today()}/{vessel_id}/{date.today()}_{vessel_id}.shp')
+
+    #polyline tracking df
+    if len(df.index) > 1:
+        try:
+            lines_gdf = geo_df.groupby(['MMSI'])['geometry'].apply(lambda x: LineString(x.tolist()))
+            lines_gdf = gpd.GeoDataFrame(lines_gdf, geometry='geometry')
+            print(f'\nlines_gdf (geodataframe): \n{lines_gdf.head()}\n')
+
+            #daily summary
+            if not os.path.exists(f'./tracking/0 {date.today()} summary.json'):
+                print('creating new summary file...')
+                lines_gdf.to_file(f'./tracking/0 {date.today()} summary.json')
+            else:
+                with open(f'./tracking/0 {date.today()} summary.json', 'r') as file:
+                    summary = json.load(file)
+                summ_df = gpd.GeoDataFrame.from_features(summary['features'])
+                print(f'\n\summ_df: \n{summ_df.head()}\n')
+
+                df = pd.concat([summ_df, lines_gdf])
+                print(f'\n\summ_df: \n{df.head()}\n')
+
+                #summ_df = gpd.GeoDataFrame(summ_df)
+                #print(f'\n\summ_df: \n{summ_df.head()}\n')
+                #summ_df.to_file(f'./tracking/0 {date.today()} summary.json')
+        finally:
+            pass
 
     basemap = gpd.read_file('./maps/ne_10m_ocean.shp')
 
